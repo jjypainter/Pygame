f01a6b622f3fff25c3aa12e1de27dc2008f7c4f8
jjypainter <jypainter@naver.com>
2020-01-27 15:37:41 +0900

    드디어 됨

--- justwindow.py
+++ justwindow.py
@@ -2,24 +2,22 @@ import sys
 import pygame
 from pygame.locals import QUIT
 
-
 pygame.init()
-SURFACE=pygame.display.set_mode([400, 300])
+SURFACE = pygame.display.set_mode([400, 300])
 pygame.display.set_caption("Just Window")
 
 
 def main():
     """main routine"""
     while True:
-        SURFACE.fill((255,255,255))
-
+        SURFACE.fill((255, 255, 255))
 
         for event in pygame.event.get():
-            if event.type==QUIT:
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             pygame.display.update()
 
-if __name__==' __main__ ':
-    main()
 
+if __name__ == '__main__':
+    main()
